f1beae9c6d814a8ec804c8fdfcea512290619eb4
dzulfikarmagang123 <fikaralghifari0504@gmail.com>
2023-09-13 10:00:01 +0700

    add Portfolio

---
+++ custom.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+  var list_detail = ["detail-carousel-1", "detail-carousel-2"];
+  var list_carousel = ["card-carousel-1", "card-carousel-2"];
+  $(() => {
+    $.each(list_detail, function (i, v) {
+      $("." + v).addClass("d-none");
+    });
+  });
+
+  $('.owl-carousel').owlCarousel({
+    center: true,
+    items:2,
+    loop:true,
+    margin:30,
+    responsive:{
+        600:{
+            items:4
+        }
+    }
+})
+
+  $.each(list_carousel, function(index, value){
+
+    $("."+value).click(() => {
+      var id = $("."+value).data("id");
+      // console.log(id);
+      // $("#detail-" + id).removeClass("d-none");
+      $.each(list_detail, function (i, v) {
+        var nameList = "detail-carousel-"+id;
+        // console.log(nameList);
+        if(v == nameList){
+          $("." + v).removeClass("d-none").addClass("d-block")  ;  
+        }else{
+          $("." + v).removeClass("d-block").addClass("d-none");
+        }
+        
+      });
+    });
+  });
+});
